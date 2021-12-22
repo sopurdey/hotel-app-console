e957d5ff0972967ffb99190e92b922f6fbff52a8
sopurdey <a_purdey@hotmail.com>
2021-12-22 16:42:58 +0100

    TP Hôtel app - rêq liste des clients

--- index.js
+++ index.js
@@ -2,3 +2,37 @@ let pres = require('./presentation');
 
 console.log('** Administration Hotel **');
 pres.start();
+
+/**
+ * Charger le module express intallé en local :
+ * npm i express (package.json)
+ * API permet de gérer les appels HTTP GET, POST, DELETE, PUT, PATCH
+ * (Server HTTP) qui attendent les requêtes Client HTTP
+ */
+// 1 Charger le module
+let express = require("express");
+
+// 2 Charger l'export (arrow function) 
+let app = express();
+
+//Middleware
+// Chargement de Gestion des API REST : flux JSON
+app.use(express.json());
+
+/**
+ * Des données en objets JSON [{}, {}]
+ */
+const clients = require('./clients.json');
+
+/**
+ * Configurer un appel HTTP GET + Mapping "/clients"
+ * localhost:8080/clients
+ *  -> le résultat sera en JSON la liste des clients
+ */
+app.get('/clients', (req, res) => {
+    res.status(200).json(clients);
+});
+
+app.listen(8080, () => {
+    //console.log("Serveur à l'écoute !")
+});
--- presentation.js
+++ presentation.js
@@ -1,11 +1,13 @@
+const { express } = require('express');
+let request = require('request');
+// récupération du module `readline`
+let readline = require('readline');
+
 exports.start = start;
 
 function start() {
     console.log('1. Lister les clients');
     console.log('99. Sortir');
-
-    // récupération du module `readline`
-    let readline = require('readline');
 
     // création d'un objet `rl` permettant de récupérer la saisie utilisateur
     let rl = readline.createInterface({
@@ -14,20 +16,31 @@ function start() {
     });
     // récupération de la saisie utilisateur
     rl.question('Merci de selectionner une option : ', function (saisie) {
-        if (saisie == 1) {
-            console.log('>> Liste des clients');
-            rl.close();
-            start();
-        } else if (saisie === '99') {
-            console.log('Au revoir');
-            rl.close();
-        } else {
-            console.log('Merci de saisir une option valable');
-            rl.close();
-            start();
+        switch (saisie) {
+            case '1':
+                displayClients();
+                rl.close();
+                break;
+            case '99':
+                console.log('Au revoir');
+                rl.close();
+            default:
+                console.log('Merci de saisir une option valable');
+                rl.close();
+                start();
+                break;
         }
+        rl.close();
     });
 }
 
-
-
+function displayClients() {
+    request('http://localhost:8080/clients', { json: true },
+        (err, res, body) => {
+            if (err) {
+                return console.log('Erreur', err);
+            }
+            console.log('>> Liste des clients', body);
+            start();
+        });
+}
